b8537c6b4b3df2c2fcfa20f41ec950ad2d91e9e2
osimen grace <osimenobehi6@gmail.com>
2024-04-20 18:52:25 +0100

    configuration

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kill process killmenow
-
-exec { 'pkill':
-	command  => 'pkill killmenow',
-	provider => 'shell',
+# Executes a command
+exec { 'pkill killmenow':
+	path => '/usr/bin:/usr/sbin:/bin'
 }
+
+
