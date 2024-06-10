1b0e30562debf884978b57cc9738db739a83db1b
osimen grace <osimenobehi6@gmail.com>
2024-06-10 22:53:12 +0100

    web debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,4 @@
-exec {'replace':
-   provide => shell,
-   command => 'sed -i "s/
-phpp/php/g" /var/www/html/wp-
-settings.php'
+exec { 'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
