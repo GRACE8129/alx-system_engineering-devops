7c03ed0b79b058c59a4ee9247f91afb7df39ac1a
osimen grace <osimenobehi6@gmail.com>
2024-06-11 14:11:01 +0100

    web debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,15 @@
-exec { 'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+# The Apache server will return error 500.
+# Find the issue, fix and then automate it
+# with Puppet (instead of Bash)
+
+# The manuscript for puppet to replace a
+# line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+# replacing the line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => '/usr/local/bin/:/bin/' # ['/bin','/usr/bin']
 }
